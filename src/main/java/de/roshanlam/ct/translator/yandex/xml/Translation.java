6ec6a5ecdd7add806e481bf69c771867a297f113
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-10-10 12:22:48 -0400

    Create Translation.java

---
+++ src/main/java/de/roshanlam/ct/translator/yandex/xml/Translation.java
@@ -0,0 +1,39 @@
+package de.dhiller.babel.translator.yandex.xml;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="Translation")
+public class Translation {
+
+    private String code;
+
+    private String encoding;
+
+    private List<String> text;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    public List<String> getText() {
+        return text;
+    }
+
+    public void setText(List<String> text) {
+        this.text = text;
+    }
+}
